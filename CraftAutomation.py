5d7b55e5858ac5f03609ec7e4328a58fcaf5aa3f
ny <hhh>
2021-05-12 01:46:23 +0800

    bugfix

--- CraftAutomation.py
+++ CraftAutomation.py
@@ -10,11 +10,12 @@ class CraftAutomation(PluginBase):
     def __init__(self):
         super().__init__()
         self.count = 0
-        self.register_event('craft_action',self.next_event)
+        self.register_event('craft_action', self.next_event)
+        self.register_event('craft_end', self.next_event)
         api.command.register(command, self.process_command)
 
-    def next_event(self,evt):
-        self.count+=1
+    def next_event(self, evt):
+        self.count += 1
 
     def _start(self):
         import XivCraft
@@ -29,8 +30,16 @@ class CraftAutomation(PluginBase):
 
     def new_callback(self, ans: str):
         temp = self.count
-        cmd = '/ac "%s"' % (ans if ans != "terminate" else '坯料加工')
+        me = api.XivMemory.actor_table.get_me()
+        if me is None: return
+        if ans != "terminate":
+            s = ans
+        elif me.currentCP >= 40:
+            s = '坯料加工'
+        else:
+            s = '仓促'
+        cmd = '/ac "%s"' % s
         api.Magic.macro_command(cmd)
         time.sleep(5)
         if self.count == temp:
-            self.new_callback(ans)
+            self.new_callback(s)
