81223df52ebcb9174ababe13fb25bae0a186aace
ny <hhh>
2021-06-27 23:41:18 +0800

    optimize

--- AFix.py
+++ AFix.py
@@ -9,6 +9,7 @@ import math
 
 ActionSendOpcode = 844  # cn5.45
 PositionSetOpcode = 0x326  # cn5.45
+PositionAdjustOpcode = 0xd7  # cn5.45
 
 FRONT = 1
 SIDE = 2
@@ -41,6 +42,15 @@ ActionSend = OffsetStruct({
     '_unk_ushort6': (c_ushort, 0xe),
     'target_id': (c_uint, 0x10),
 }, 32)
+PositionAdjustPack = OffsetStruct({
+    'old_r': (c_float, 0x0),
+    'new_r': (c_float, 0x4),
+    'unk0': (c_ushort, 0x8),
+    'unk1': (c_ushort, 0xA),
+    'old_pos': (Vector3, 0xC),
+    'new_pos': (Vector3, 0x18),
+    'unk2': (c_uint, 0x24),
+}, 40)
 
 angle = math.pi / 2 - 0.1
 
@@ -72,18 +82,36 @@ class AFix(PluginBase):
         self.last_reset = perf_counter()
         api.XivNetwork.register_makeup(ActionSendOpcode, self.makeup_action)
         self.register_event(f'network/action_effect', self.coor_return)
+        self.adjust_mode = True
+        self.adjust_sig = 0
+        self.register_event('network/position_adjust', self.deal_adjust)
+        self.register_event('network/position_set', self.deal_set)
 
     def _onunload(self):
         api.XivNetwork.unregister_makeup(ActionSendOpcode, self.makeup_action)
 
+    def deal_adjust(self, evt):
+        self.adjust_mode = True
+        self.adjust_sig = evt.raw_msg.unk1 & 0xf
+
+    def deal_set(self, evt):
+        self.adjust_mode = False
+
+    def goto(self, new_x=None, new_y=None):
+        c = api.Coordinate()
+        target = Vector3(x=new_x if new_x is not None else c.x, y=new_y if new_y is not None else c.y, z=c.z)
+        if self.adjust_mode:
+            msg = PositionAdjustPack(old_r=c.r, new_r=c.r, old_pos=target, new_pos=target, unk0=0x4000, unk1=0x40 | self.adjust_sig)
+            code = PositionAdjustOpcode
+        else:
+            msg = PositionSetPack(r=c.r, pos=target, unk2=0x93)
+            code = PositionSetOpcode
+        api.XivNetwork.send_messages([(code, bytearray(msg))], False)
+
     def coor_return(self, evt):
         if self.last_reset + 1 > perf_counter() or evt.source_id != api.XivMemory.actor_table.get_me().id or evt.action_type != 'action' or evt.action_id not in skills:
             return
-        c = api.Coordinate()
-        frame_inject.register_once_call(
-            api.XivNetwork.send_messages,
-            [(PositionSetOpcode, bytearray(PositionSetPack(r=c.r, pos=Vector3(x=c.x, y=c.y, z=c.z), unk2=0x93)))]
-        )
+        frame_inject.register_once_call(self.goto)
         self.last_reset = perf_counter()
 
     def makeup_action(self, header, raw):
@@ -91,7 +119,5 @@ class AFix(PluginBase):
         if d.skill_id in skills:
             t = api.XivMemory.actor_table.get_actor_by_id(d.target_id)
             if t is not None:
-                new_x, new_y = get_nearest(api.Coordinate(), t, skills[d.skill_id])
-                msg = PositionSetPack(r=t.pos.r, pos=Vector3(x=new_x, y=new_y, z=t.pos.z), unk2=0x93)
-                api.XivNetwork.send_messages([(PositionSetOpcode, bytearray(msg))], False)
+                self.goto(*get_nearest(api.Coordinate(), t, skills[d.skill_id]))
         return header, bytearray(d)
