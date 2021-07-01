cd43d5ef53d99598ca66ec9f0b25fc1799d39c2e
ny <hhh>
2021-07-02 01:11:47 +0800

    add auto keys

--- CutTheTree.py
+++ CutTheTree.py
@@ -11,6 +11,11 @@ send_opcode = 0x39d  # cn5.45
 # recv_opcode = 789  # cn5.41
 # send_opcode = 843  # cn5.41
 
+KEY_CONFIRM = 96
+KEY_CANCEL = 110
+KEY_LEFT = 100
+KEY_UP = 104
+
 recv_packet = OffsetStruct({
     'cut_result': (EnumStruct(c_ubyte, {
         0x0: "Fail",
@@ -93,10 +98,18 @@ class CutTheTree(PluginBase):
         self.backup_fell = None
         self.backup_next = None
 
+        global KEY_UP, KEY_CONFIRM, KEY_CANCEL, KEY_LEFT
+        KEY_UP = self.storage.data.setdefault("KEY_UP", 104)
+        KEY_CONFIRM = self.storage.data.setdefault("KEY_CONFIRM", 96)
+        KEY_CANCEL = self.storage.data.setdefault("KEY_CANCEL", 110)
+        KEY_LEFT = self.storage.data.setdefault("KEY_LEFT", 100)
+        self.storage.save()
+
         self.solver = Solver()
 
-        self.register_event(f'network/recv/{recv_opcode}', self.recv_work)
-        self.register_event(f'network/send/{send_opcode}', self.send_work)
+        self.register_event(f'network/recv/{recv_opcode}', self.recv_work, limit_sec=0)
+        self.register_event(f'network/send/{send_opcode}', self.send_work, limit_sec=0)
+        self.register_event('network/undefined_recv/InventoryActionAck', self.start_new, limit_sec=0)
         api.XivNetwork.register_makeup(send_opcode, self.makeup_data)
         api.command.register(command, self.process_command)
 
@@ -119,6 +132,22 @@ class CutTheTree(PluginBase):
     def send(self, msg):
         frame_inject.register_once_call(api.XivNetwork.send_messages, [(send_opcode, bytearray(msg))])
 
+    def start_new(self, evt):
+        if self.enable:
+            sleep(1)
+            self.logger.debug("new game")
+            for i in range(5):
+                api.SendKeys.key_press(KEY_CANCEL, 100)  # cancel
+            target = find_nearest_tree()
+            if target is not None:
+                # self.logger.debug(target)
+                api.XivMemory.targets.set_current(target)
+                api.SendKeys.key_press(KEY_CONFIRM)  # confirm
+                api.SendKeys.key_press(KEY_CONFIRM)  # confirm
+                sleep(1)
+                api.SendKeys.key_press(KEY_UP)  # up
+                api.SendKeys.key_press(KEY_CONFIRM)  # confirm
+
     def send_fell(self):
         if self.backup_fell is not None:
             ans = self.solver.solve()
@@ -140,6 +169,8 @@ class CutTheTree(PluginBase):
                 self.send(self.backup_next)
             else:
                 api.Magic.echo_msg("Cut!")
+                for i in range(5):
+                    api.SendKeys.key_press(KEY_CONFIRM)
 
     def send_work(self, event):
         data = send_packet.from_buffer(event.raw_msg)
@@ -148,15 +179,16 @@ class CutTheTree(PluginBase):
             msg = f"{msg} << {data.param}"
         self.logger.debug(msg)
         key = data.game_state.value()
+        if msg == "Start Game" and self.enable:
+            sleep(3)
+            for i in range(5):
+                api.SendKeys.key_press(KEY_CONFIRM)
         if key == "Difficulty choice" or key == "Start Next Round":
             self.solver.reset()
         if key == "Start Next Round":
             self.backup_next = data
             if self.enable:
                 self.send_fell()
-        # elif key == "Difficulty choice":
-        #     if self.enable:
-        #         self.send_fell()
         elif key == "Felling":
             self.backup_fell = data
 
@@ -172,28 +204,17 @@ class CutTheTree(PluginBase):
                 data.param = ans
         return header, bytearray(data)
 
-# import math
-#
-# NPC_Name = "孤树无援"
-#
-#
-# def dis(a, b):
-#     return math.sqrt((a.x - b.x) ** 2 + (a.y - b.y) ** 2 + (a.z - b.z) ** 2)
-#
-#
-# def find_nearest_tree():
-#     nearest = [None, ]
-#     nearest_dis = 9999
-#     me_pos = api.XivMemory.actor_table.get_me().pos
-#
-#     for i, a1, a2 in api.XivMemory.actor_table.get_item():
-#         dis1 = dis(me_pos, a1.pos)
-#         if dis1 < nearest_dis:
-#             nearest[0] = a1
-#             nearest_dis = dis1
-#         if a2 is not None:
-#             dis2 = dis(me_pos, a2.pos)
-#             if dis2 < nearest_dis:
-#                 nearest[0] = a2
-#                 nearest_dis = dis2
-#     return nearest[0]
+
+NPC_Name = "孤树无援"
+
+
+def find_nearest_tree():
+    nearest = None
+    nearest_dis = 9999
+    me = api.XivMemory.actor_table.get_me()
+    for a1 in api.XivMemory.actor_table.get_actors_by_name(NPC_Name):
+        dis1 = me.absolute_distance_xy(a1)
+        if dis1 < nearest_dis:
+            nearest = a1
+            nearest_dis = dis1
+    return nearest
