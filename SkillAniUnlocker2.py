3504b99c924419e2a4c43c81d34aebc28216d1d1
ny <hhh>
2021-07-10 20:15:47 +0800

    add local set

--- SkillAniUnlocker2.py
+++ SkillAniUnlocker2.py
@@ -3,11 +3,12 @@ from ctypes import *
 from FFxivPythonTrigger import PluginBase, api
 from FFxivPythonTrigger.AddressManager import AddressManager
 from FFxivPythonTrigger.hook import Hook
-from FFxivPythonTrigger.memory import read_memory, scan_pattern
+from FFxivPythonTrigger.memory import read_memory, scan_pattern, write_float
 from FFxivPythonTrigger.memory.StructFactory import OffsetStruct
 
 command = "@sALock"
 sig = "4C 89 44 24 ? 53 56 57 41 54 41 57"
+sig_fix = "41 C7 45 08 ? ? ? ? EB ? 41 C7 45 08"
 
 a4_struct = OffsetStruct({
     'action_id': (c_ushort, 28),
@@ -17,6 +18,9 @@ a4_struct = OffsetStruct({
 
 DEFAULT_LOCK_TIME = 0.6
 DEFAULT_HACK_LOCK = 0.15
+
+DEFAULT_FIX1 = 0.35
+DEFAULT_FIX2 = 0.5
 
 
 class SkillAniUnlocker2(PluginBase):
@@ -41,7 +45,9 @@ class SkillAniUnlocker2(PluginBase):
 
         self.lock_time = DEFAULT_HACK_LOCK
         self.enable = False
-        self.hook = ActionHook(AddressManager(self.storage.data, self.logger).get('addr', scan_pattern, sig))
+        am = AddressManager(self.storage.data, self.logger)
+        self.hook = ActionHook(am.get('addr', scan_pattern, sig))
+        self.fix_addr = am.get('fix', scan_pattern, sig_fix)
         self.storage.save()
         api.command.register(command, self.process_command)
 
@@ -64,14 +70,18 @@ class SkillAniUnlocker2(PluginBase):
             if len(args):
                 if args[0] == "patch" or args[0] == "p":
                     self.enable = True
-                    if len(args) > 1: self.lock_time = float(args[1])
-                    else: self.lock_time = DEFAULT_HACK_LOCK
+                    if len(args) > 1:
+                        self.lock_time = float(args[1])
+                    else:
+                        self.lock_time = DEFAULT_HACK_LOCK
                 elif args[0] == "dispatch" or args[0] == "d":
                     self.enable = False
                 else:
                     return "unknown arguments {}".format(args[0])
             else:
                 self.enable = not self.enable
+            write_float(self.fix_addr + 4, min(self.lock_time, DEFAULT_FIX1) if self.enable else DEFAULT_FIX1)
+            write_float(self.fix_addr + 14, min(self.lock_time, DEFAULT_FIX2) if self.enable else DEFAULT_FIX2)
             return self.status()
         except Exception as e:
             return str(e)
