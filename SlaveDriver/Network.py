7d3c969ec0908898c7a62c636c33688c96eaf5e7
ny <hhh>
2021-07-26 03:13:31 +0800

    test ver

---
+++ SlaveDriver/Network.py
@@ -0,0 +1,96 @@
+from ctypes import *
+
+from FFxivPythonTrigger import api
+
+from FFxivPythonTrigger.memory.StructFactory import OffsetStruct
+
+ListReadyOpcode = 262
+AdventureConfirmedOpcode = 842
+
+ClientEventStart = OffsetStruct({
+    'target_id': c_uint,
+    'unk0': c_uint,
+    'event_id': c_ushort,
+    'category': c_ushort,
+    'unk3': c_uint,
+}, 16)
+
+ClientEventFinish = OffsetStruct({
+    'event_id': c_ushort,
+    'category': c_ushort,
+    'unk2': c_uint,
+    'unk3': c_uint,
+    'unk4': c_uint,
+}, 16)
+
+ClientTrigger = OffsetStruct({
+    'param1': c_uint,
+    'param2': c_uint,
+    'param3': c_uint,
+    'param4': c_uint,
+    'param5': c_uint,
+    'param6': c_uint,
+    'param7': c_uint,
+    'param8': c_uint,
+}, 32)
+
+ask_list_trigger = ClientTrigger(param1=0x232c, param3=2, param6=0x40c66666)
+confirm_retainer_hello_msg = ClientTrigger(param1=0x232c, param3=4, param6=0xac)
+confirm_adventure_msg = ClientEventFinish(event_id=544, category=11, unk3=7, unk2=0x2000002)
+finish_sending_adventure_msg = ClientEventFinish(event_id=544, category=11, unk2=3)
+finish_sending_adventure_msg2 = ClientTrigger(param1=0x232c, param3=4, param6=0x7ff6)
+finish_retainer_msg = ClientEventFinish(event_id=544, category=11, unk2=2)
+close_list_msg = ClientEventFinish(event_id=544, category=11)
+
+
+def is_start_list_response(evt):
+    return evt.raw_msg.category == 11 and evt.raw_msg.event_id == 544
+
+
+def start_list(target_id):
+    msg = ClientEventStart(target_id=target_id, event_id=544, category=11)
+    return api.XivNetwork.send_messages([("EventStart", bytearray(msg))], response_opcode="EventPlay", response_statement=is_start_list_response)
+
+
+def ask_list():
+    return api.XivNetwork.send_messages([("ClientTrigger", bytearray(ask_list_trigger))], response_opcode=ListReadyOpcode)
+
+
+def start_retainer(retainer_id, server_id, is_continue=False):
+    msg = ClientEventFinish(event_id=544, category=11, unk3=server_id, unk4=retainer_id, unk2=0x2000000 + int(is_continue))
+    return api.XivNetwork.send_messages([("EventFinish", bytearray(msg))], response_opcode="EventPlay4")
+
+
+def confirm_retainer_hello():
+    api.XivNetwork.send_messages([("ClientTrigger", bytearray(confirm_retainer_hello_msg))])
+
+
+def confirm_adventure():
+    return api.XivNetwork.send_messages([("EventFinish", bytearray(confirm_adventure_msg))], response_opcode=AdventureConfirmedOpcode)
+
+
+def resend_adventure(mission_id):
+    msg = ClientTrigger(param1=0x12c, param2=mission_id, param6=0x7ff6)
+    return api.XivNetwork.send_messages([("ClientTrigger", bytearray(msg))], response_opcode='Examine')
+
+
+def confirm_retainer_go(mission_type):
+    msg = ClientTrigger(param1=0x232c, param3=5, param4=mission_type)
+    api.XivNetwork.send_messages([("ClientTrigger", bytearray(msg))])
+
+
+def finish_sending_adventure():
+    return api.XivNetwork.send_messages([("EventFinish", bytearray(finish_sending_adventure_msg))], response_opcode="EventPlay4")
+
+
+def finish_sending_adventure2():
+    return api.XivNetwork.send_messages([("ClientTrigger", bytearray(finish_sending_adventure_msg2))])
+
+
+def finish_retainer():
+    return api.XivNetwork.send_messages([("EventFinish", bytearray(finish_retainer_msg))], response_opcode="EventPlay",
+                                        response_statement=is_start_list_response)
+
+
+def close_list():
+    api.XivNetwork.send_messages([("EventFinish", bytearray(close_list_msg))])
---
+++ SlaveDriver/__init__.py
@@ -0,0 +1,88 @@
+import time
+from ctypes import *
+
+from FFxivPythonTrigger import PluginBase, api
+from FFxivPythonTrigger.memory.StructFactory import OffsetStruct
+from . import Network
+
+bell_name = "传唤铃"
+
+AdventureData = OffsetStruct({
+    'mission_id': (c_uint, 0),
+    'next_mission_id': (c_uint, 0x1c),
+    'mission_type': (c_uint, 0x28),
+})
+
+
+def find_bell_id() -> int:
+    me = api.XivMemory.actor_table.get_me()
+    for actor in api.XivMemory.actor_table.get_actors_by_name(bell_name):
+        if actor.type == 12 and me.absolute_distance_xy(actor) < 5:
+            return actor.id
+    raise Exception("No bell found")
+
+
+class SlaveDriver(PluginBase):
+    name = "SlaveDriver"
+
+    def __init__(self):
+        super().__init__()
+        self.working = False
+        self.retainers_finished = list()
+        self.register_event("network/recv_retainer_info", self.recv_retainer_info)
+        api.XivNetwork.register_makeup("EventFinish", self.def_finish)
+        api.XivNetwork.register_makeup("ClientTrigger", self.def_trigger)
+
+    def _start(self):
+        self.start_mission()
+
+    def _onunload(self):
+        api.XivNetwork.unregister_makeup("EventFinish", self.def_finish)
+        api.XivNetwork.unregister_makeup("ClientTrigger", self.def_trigger)
+
+    def recv_retainer_info(self, event):
+        msg = event.raw_msg
+        if not msg.reserved: return
+        retainer = (msg.retainer_id, msg.server_id)
+        if msg.adv_end_time and msg.adv_end_time < time.time():
+            self.logger(msg.name,1)
+            self.retainers_finished.append(retainer)
+        else:
+            self.logger(msg.name, 0)
+            if retainer in self.retainers_finished:
+                self.retainers_finished.remove(retainer)
+
+    def def_finish(self, header, raw):
+        return header, bytearray(Network.ClientEventFinish()) if self.working else raw
+
+    def def_trigger(self, header, raw):
+        return header, bytearray(Network.ClientTrigger()) if self.working else raw
+
+    def start_mission(self):
+        self.working = True
+        try:
+            self._start_mission()
+        finally:
+            self.working = False
+
+    def _start_mission(self):
+        Network.start_list(find_bell_id())
+        Network.ask_list()
+        time.sleep(0.05)
+        cnt = 0
+        while self.retainers_finished:
+            retainer_id, server_id = self.retainers_finished.pop()
+            Network.start_retainer(retainer_id, server_id,bool(cnt))
+            Network.confirm_retainer_hello()
+            res = Network.confirm_adventure()
+            data=AdventureData.from_buffer(res.raw_msg)
+            Network.resend_adventure(data.next_mission_id)
+            Network.confirm_retainer_go(data.mission_type)
+            Network.finish_sending_adventure()
+            Network.finish_sending_adventure2()
+            Network.finish_retainer()
+            Network.ask_list()
+            cnt+=1
+            time.sleep(0.05)
+        Network.close_list()
+
